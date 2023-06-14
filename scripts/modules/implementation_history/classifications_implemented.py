7f2c4b71043dcf69bdddfa738b7fb9e09be94b1f
hannahsallen <13205177+hannahsallen@users.noreply.github.com>
2023-06-14 10:26:46 -0700

    update

--- scripts/modules/implementation_history/classifications_implemented.py
+++ scripts/modules/implementation_history/classifications_implemented.py
@@ -1,17 +1,20 @@
-{
+##! /usr/bin/env python3
+
+
+CLASSIFICATIONS_IMPLEMENTED = {
   "Classifications": [
     {
       "name": "Total Stock",
       "rule_name": "Total_Stock",
-      "created": true,
-      "connected_to_glossary_term": false,
+      "created": True,
+      "connected_to_glossary_term": False,
       "regex": "/\b.*Total Stock.*\b/|Total Stock|/\b.*Total Inventory.*\b/|Total Inventory|/\b.*TOT.*\b/|TOT|/\b.*INV.*\b/|INV|/\b.*TOTL.*\b/|TOTL|/\b.*STK.*\b/|STK"
     },
     {
       "name": "Volume",
       "rule_name": "Volume",
-      "created": true,
-      "connected_to_glossary_term": false,
+      "created": True,
+      "connected_to_glossary_term": False,
       "regex": "/\.*VOLUME.*\b/|VoLume|/\.*VOL.*\b/|vol"
     }
   ]
