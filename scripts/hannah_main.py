2161106c19823aa55bbe41003d012ab360fd1405
hannahsallen <13205177+hannahsallen@users.noreply.github.com>
2023-06-20 06:50:17 -0700

    update

--- scripts/hannah_main.py
+++ scripts/hannah_main.py
@@ -42,14 +42,15 @@ MAPPINGS_EXCEL_FILE = "Keyword_to_Common_Abbreviation_Mappings.xlsx"
 def main():
     print()
 
+    '''
     profit_center_regex = r"/\b.*Profit[^A-Za-z0-9]?(Center|CTR).*\b/|Profit[^A-Za-z0-9]?(Center|CTR)"
     storage_location_regex = r"/\b.*Storage[^A-Za-z0-9]?(Location|LOC).*\b/|Storage[^A-Za-z0-9]?(Location|LOC)|/\b.*Warehouse.*\b/|Warehouse|/\b.*SLOC.*\b/|SLOC"
+    '''
     
-    
-    """
+    '''
     pass_file_names = generate_all_pass_test_files(CLASSIFICATION_EXCEL_FILE, MAPPINGS_EXCEL_FILE)
     print(pass_file_names)
-    """
+    '''
 
     '''
     valuation_class_pass_file_name = 'test_CSVs/to_pass/material_type_to_pass_test_column_names.csv'
@@ -58,10 +59,31 @@ def main():
     print(len(column_names))
     '''
 
+    '''
     all_regex = generate_all_regex(CLASSIFICATION_EXCEL_FILE, MAPPINGS_EXCEL_FILE)
     print(all_regex)
+    '''
 
+    all_type_defs = CLIENT.get_all_typedefs()
+    entity_defs = all_type_defs["entityDefs"]
+    all_type_names = []
 
+    for entity in entity_defs:
+        rel_attr_defs = entity["relationshipAttributeDefs"]
+        for td in rel_attr_defs:
+            all_type_names.append(td["relationshipTypeName"])
+    unique_type_names = list(set(all_type_names))
+
+    sap_types = []
+    for name in unique_type_names:
+        if "sap" in name:
+            sap_types.append(name)
+        
+    sap_s4hana_types = []
+    for name in sap_types:
+        if "sap_s4hana" in name:
+            sap_s4hana_types.append(name)
+    print(sap_s4hana_types)
 
 
     
