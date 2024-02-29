d21c67a7951ceb9b0133dabfdabe2aaed0560030
Srdasam <Sravanthi.Dasam@hanes.com>
2024-02-29 19:58:35 +0530

    Added changes

--- scripts/modules/glossary_propagation/sap_hana_glossary_propagation.py
+++ scripts/modules/glossary_propagation/sap_hana_glossary_propagation.py
@@ -24,15 +24,18 @@ from pathlib import Path
 import pandas as pd
 
 
-
 # Constants
 # ---------------
 
 REFERENCE_NAME_PURVIEW = "hbi-qa01-datamgmt-pview"
 PROJ_PATH = Path(__file__).resolve().parent
 CREDS = get_credentials(cred_type= 'default')
-CLIENT = create_purview_client(credentials=CREDS, mod_type='pyapacheatlas', purview_account= REFERENCE_NAME_PURVIEW)
+qa_client = create_purview_client(credentials=CREDS, mod_type='pyapacheatlas', purview_account= REFERENCE_NAME_PURVIEW)
 
+REFERENCE_NAME_PURVIEW = "hbi-pd01-datamgmt-pview"
+PROJ_PATH = Path(__file__).resolve().parent
+CREDS = get_credentials(cred_type= 'default')
+prod_client = create_purview_client(credentials=CREDS, mod_type='pyapacheatlas', purview_account= REFERENCE_NAME_PURVIEW)
 
 # Functions
 # ---------------
