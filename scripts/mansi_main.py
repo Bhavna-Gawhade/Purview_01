aa57914d4aedb92491603bfc43b5ab2a34156927
Hannah Allen <hannah.allen@hanes.com>
2024-04-19 09:48:40 -0700

    update

--- scripts/mansi_main.py
+++ scripts/mansi_main.py
@@ -4,39 +4,7 @@
 # Import Functions
 # ---------------
 
-from modules import entity
-from modules.classification.test_case_generator import *
-from modules.classification.regex_generator import *
-from modules.classification.shared_generator_functions import *
-from modules.classification.classification import *
-
-from modules.lineage.cube_lineage import *
-from modules.lineage.databricks_lineage import *
-from modules.lineage.data_lake_lineage import *
-from modules.lineage.data_warehouse_internal_lineage import *
-from modules.lineage.json_payload_lineage import *
-from modules.lineage.informatica_lineage import *
-from modules.lineage.oracle_server_lineage import *
-from modules.lineage.pkms_lineage import *
-from modules.lineage.powerbi_sql_query_lineage import *
-from modules.lineage.analysis_services_tabular_model_lineage import *
-from modules.lineage.sap_hana_internal_lineage import *
-from modules.lineage.shared_lineage_functions import *
-from modules.lineage.sharepoint_lineage import *
-from modules.lineage.sql_server_lineage import *
-
-from modules.glossary_propagation.shared_glossary_functions import *
-from modules.glossary_propagation.sap_hana_glossary_propagation import *
-from modules.glossary_propagation.sap_s4hana_glossary_propagation import *
-from modules.glossary_propagation.datalake_glossary_propagation import *
-from modules.glossary_propagation.sql_dw_glossary_propagation import *
-from modules.entity import *
-from modules.collection.collection_shared_functions import *
-from modules.collection.sap_s4hana_collection_sorting import *
-from modules.collection.azure_dw_collection_sorting import *
-from utils import get_credentials, create_purview_client
 from pyapacheatlas.core.util import GuidTracker
-
 from pyapacheatlas.readers import ExcelConfiguration, ExcelReader
 
 
@@ -44,15 +12,77 @@ from pyapacheatlas.readers import ExcelConfiguration, ExcelReader
 # ---------------
 
 from pathlib import Path
-from pyapacheatlas.core.typedef import AtlasAttributeDef, AtlasStructDef, TypeCategory, AtlasRelationshipAttributeDef
 from pyapacheatlas.core import AtlasEntity, AtlasProcess
 from azure.core.exceptions import HttpResponseError
-import json
 from datetime import datetime
-import re
-import time
-import sys
-from pyapacheatlas.readers import ExcelConfiguration, ExcelReader
+from pyapacheatlas.core import PurviewClient
+from pyapacheatlas.auth import ServicePrincipalAuthentication
+from pyapacheatlas.core import PurviewClient
+from pathlib import Path
+from azure.identity import ClientSecretCredential, DefaultAzureCredential
+from typing import Union
+
+
+# Credential Functions
+# ---------------
+
+# Functions
+# ---------------
+
+def get_credentials(cred_type: str, client_id: str = None, client_secret: str = None, tenant_id: str = None) -> Union[DefaultAzureCredential, ClientSecretCredential, ServicePrincipalAuthentication]:
+    """
+    Returns either a DefaultAzureCredential or ClientSecretCredential based on the provided arguments.
+
+    Args:
+        cred_type (str): The type of the credentials to create. Can be 'default' or 'client_secret'.
+        client_id (str, optional): The client ID of the service principal. Required if cred_type is 'client_secret'.
+        client_secret (str, optional): The client secret of the service principal. Required if cred_type is 'client_secret'.
+        tenant_id (str, optional): The tenant ID of the service principal. Required if cred_type is 'client_secret'.
+
+    Returns:
+        Union[DefaultAzureCredential, ClientSecretCredential]: The created credentials.
+
+    Raises:
+        ValueError: If the provided cred_type is not supported or if cred_type is 'client_secret' but client_id, client_secret, or tenant_id are not provided.
+    """
+    if cred_type == 'default':
+        return DefaultAzureCredential(exclude_shared_token_cache_credential=True)
+    elif cred_type == 'client_secret':
+        if not all([client_id, client_secret, tenant_id]):
+            raise ValueError("client_id, client_secret, and tenant_id are required when cred_type is 'client_secret'.")
+        return ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
+    elif cred_type == 'pyapacheatlas_secret':
+        if not all([client_id, client_secret, tenant_id]):
+            raise ValueError("client_id, client_secret, and tenant_id are required when cred_type is 'client_secret'.")
+        return ServicePrincipalAuthentication(tenant_id=tenant_id,client_id=client_id,client_secret=client_secret)
+    else:
+        raise ValueError(f"Invalid cred_type provided: {cred_type}")
+    
+
+def create_purview_client(credentials: Union[DefaultAzureCredential, ClientSecretCredential, ServicePrincipalAuthentication], purview_account: str, mod_type: str) -> PurviewClient:
+    """
+    Creates and returns a PurviewClient object authenticated with Azure AD Service Principal.
+
+    Args:
+        credentials (Union[DefaultAzureCredential, ClientSecretCredential, ServicePrincipalAuthentication]): The credentials for authentication.
+        purview_account (str): The name of the Azure Purview account.
+        mod_type (str): The type of python module to use for the operations.
+
+    Returns:
+        PurviewClient: An authenticated PurviewClient object.
+
+    Raises:
+        ValueError: If an unsupported module type is specified.
+        Exception: If an error occurs during the PurviewClient instantiation.
+    """
+    try:
+        # Instantiate the PurviewClient
+        if mod_type == 'pyapacheatlas':
+            return PurviewClient(account_name=purview_account, authentication=credentials)
+        else:
+            raise ValueError("Unsupported module type: " + mod_type)
+    except Exception as e:
+        raise Exception("Error occurred during PurviewClient instantiation.") from e
 
 
 # Constants
@@ -68,10 +98,6 @@ PROJ_PATH = Path(__file__).resolve().parent
 CREDS = get_credentials(cred_type= 'default')
 prod_client = create_purview_client(credentials=CREDS, mod_type='pyapacheatlas', purview_account= REFERENCE_NAME_PURVIEW)
     
-
-
-# Functions
-# ---------------
 
 def in_progress_get_entity_from_qualified_name(client, qualified_name):
     """
